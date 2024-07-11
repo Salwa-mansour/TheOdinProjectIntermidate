ed0283f28feed483a84f191afd0c0703ee48af10
Salwa-mansour <alqadasisal@gmail.com>
2024-07-11 18:23:39 +0300

    library delete and changeReadState functions are working

--- library/app.js
+++ library/app.js
@@ -1,7 +1,7 @@
 const myLibrary = [];
 const formContainer = document.querySelector('.form-container');
 const form1 = formContainer.querySelector('.form1');
-const booksContainer = formContainer.querySelector('.books table tbody')
+const booksContainer = formContainer.querySelector('.books table tbody');
 
 function Book(title,author,pages,read) {
   // the constructor...
@@ -9,6 +9,10 @@ function Book(title,author,pages,read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
+
+  this.changReadState = function(){
+    this.read=!this.read;
+  }
 }
 
 function addBookToLibrary(e) {
@@ -34,11 +38,33 @@ function populateBooks(bookList,books){
         <td>${book.title}</td>
         <td>${book.author}</td>
         <td>${book.pages}</td>
-        <td><input type="checkbox" ${book.read ? "checked":""} ></td>
-        <td><button >delete</button></td>
+        <td><input type="checkbox" data-read-index="${i}" ${book.read ? "checked":""} ></td>
+        <td><button class="delete" data-delete-index="${i}" >delete</button></td>
     </tr>
     `;
   }).join("");
 
 }
+function deleteItem(index){
+  myLibrary.splice(index,1);
+  let li=booksContainer.querySelector(`tr[data-index="${index}"]`);
+   populateBooks(booksContainer,myLibrary)
+}
+
 form1.addEventListener('submit',addBookToLibrary);
+booksContainer.addEventListener('click',function(e){
+  if(e.target.matches('.delete')){
+      //call delete funciotn
+      let index =parseInt(e.target.dataset.deleteIndex);
+    deleteItem(index)
+  }else if(e.target.matches('input[type="checkbox"]')){
+    //call change read state funcions
+    let index=parseInt(e.target.dataset.readIndex);
+    let book = myLibrary[index]
+    book.changReadState();
+    console.log(myLibrary)
+  }
+  console.log(e.target)
+},{
+  capture:true,
+})
